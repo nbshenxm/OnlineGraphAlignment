95ac1fb80999b31086aff78d78d551691b926800
li_zhenyuan <li_zhenyuan@qq.com>
2023-07-23 21:18:25 +0800

    Add Utils

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Properties;
+
+public class Utils {
+    public static Properties loadProperties(String propertyFilePath) throws IOException {
+        InputStream inputStream = new FileInputStream(propertyFilePath);
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        inputStream.close();
+
+        return properties;
+    }
+
+    public static Properties loadInnerProperties(String propertyFilePath) throws IOException {
+        InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(propertyFilePath);
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        inputStream.close();
+
+        return properties;
+    }
+
+    public static int iterableCount(Iterator<?> it) {
+        int count = 0;
+        while (it.hasNext()) {
+            it.next();
+            count++;
+        }
+        return count;
+    }
+
+    public static String convertIntToIpString(int ip) {
+        String ipStr = String.format("%d.%d.%d.%d",
+                (ip >> 24 & 0xff),
+                (ip >> 16 & 0xff),
+                (ip >> 8 & 0xff),
+                (ip & 0xff));
+        return ipStr;
+    }
+
+    public static String parseTimeStamp(Long timeStamp) {
+        return new Date(timeStamp/1000000).toString();
+    }
+}
+
