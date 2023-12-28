e896546ae90b935376668caaa3096dc32cd371e2
wyyanhui <137664580+wyyanhui@users.noreply.github.com>
2023-12-28 21:13:34 +0800

    Assign an alert threshold for each query graph

--- src/main/java/libtagpropagation/graphalignment/alignmentstatus/GraphAlignmentStatus.java
+++ src/main/java/libtagpropagation/graphalignment/alignmentstatus/GraphAlignmentStatus.java
@@ -18,7 +18,7 @@ public class GraphAlignmentStatus {
     public String techniqueName;
     public Float alignmentScore = 0.0F;
     public boolean ALERT_FLAG;
-    public static final Float ALIGNMENT_THRESHOLDS = 1.0F;
+    public Float  ALIGNMENT_THRESHOLDS;
 
     private int nodeCount;
     private int edgeCount;
@@ -30,7 +30,7 @@ public class GraphAlignmentStatus {
         nodeCount = Iterators.size(entry.f1.tinkerGraph.getVertices().iterator());
         nodeAlignmentStatusList = new NodeAlignmentStatus[nodeCount];
         Arrays.fill(nodeAlignmentStatusList, null);
-        nodeAlignmentStatusList[entry.f0.getId()] = new NodeAlignmentStatus(entry.f0.getType(), entry.f0.getAlignedString());
+        nodeAlignmentStatusList[entry.f0.getId()] = new NodeAlignmentStatus(entry.f0.getType(), entry.f0.getAlignedString(), entry.f0.getId());
 
         edgeCount = Iterators.size(entry.f1.tinkerGraph.getEdges().iterator());
         edgeAlignmentStatusList = new EdgeAlignmentStatus[edgeCount];
@@ -38,26 +38,31 @@ public class GraphAlignmentStatus {
 
         this.techniqueName = entry.f1.techniqueName;
         this.ALERT_FLAG = false;
+        this.ALIGNMENT_THRESHOLDS = entry.f1.Alert_Threshold;
     }
 
-    public GraphAlignmentStatus tryUpdateStatus(int nodeIndex, int edgeIndex, NodeAlignmentStatus newNodeAlignmentStatus, ArrayList<AssociatedEvent> cachedPath) {
+    public GraphAlignmentStatus tryUpdateStatus(NodeAlignmentStatus sourceNodeAlignmentStatus, NodeAlignmentStatus sinkNodeAlignmentStatus, Integer edgeIndex, ArrayList<AssociatedEvent> cachedPath) {
+        Integer nodeIndex = sinkNodeAlignmentStatus.getIndex();
         if (nodeIndex >= nodeCount || edgeIndex >= edgeCount) throw new RuntimeException("This node or edge seems not in the TKG.");
 
         // ToDo：考虑边的匹配状态
         if (edgeAlignmentStatusList[edgeIndex] == null){
-            this.nodeAlignmentStatusList[nodeIndex] = newNodeAlignmentStatus;//Fixme: 节点未必为空
+            this.nodeAlignmentStatusList[nodeIndex] = sinkNodeAlignmentStatus;//Fixme: 节点未必为空
             this.edgeAlignmentStatusList[edgeIndex] = new EdgeAlignmentStatus(cachedPath, nodeIndex);
-            this.alignmentScore += newNodeAlignmentStatus.getAlignmentScore() * (1.0f / cachedPath.size() + 1) / this.edgeCount;
+            this.alignmentScore += sinkNodeAlignmentStatus.getAlignmentScore() * (1.0f / cachedPath.size() + 1) / this.edgeCount;
             this.edgeAlignmentStatusList[edgeIndex].setAnomlyScore((1.0f / cachedPath.size() + 1) / this.edgeCount);
+//            if(this.alignmentScore == 0){
+                this.nodeAlignmentStatusList[sourceNodeAlignmentStatus.getIndex()] = sourceNodeAlignmentStatus;
+//            }
         }
         else{
 //            Float newEdgeAlignmentScore = newNodeAlignmentStatus.getAlignmentScore() / cachedPath.size();
 //            Float originalAlignmentScore = this.nodeAlignmentStatusList[nodeIndex].getAlignmentScore() / this.edgeAlignmentStatusList[edgeIndex].getPathLength();
-            Float newEdgeAlignmentScore = newNodeAlignmentStatus.getAlignmentScore() * (1.0f / cachedPath.size() + 1) / this.edgeCount;
+            Float newEdgeAlignmentScore = sinkNodeAlignmentStatus.getAlignmentScore() * (1.0f / cachedPath.size() + 1) / this.edgeCount;
             Float originalAlignmentScore = this.nodeAlignmentStatusList[nodeIndex].getAlignmentScore() * this.edgeAlignmentStatusList[edgeIndex].getAnomlyScore();
             if (newEdgeAlignmentScore > originalAlignmentScore){
                 this.edgeAlignmentStatusList[edgeIndex] = new EdgeAlignmentStatus(cachedPath, nodeIndex);
-                this.nodeAlignmentStatusList[nodeIndex] = newNodeAlignmentStatus;
+                this.nodeAlignmentStatusList[nodeIndex] = sinkNodeAlignmentStatus;
                 this.alignmentScore = this.alignmentScore - originalAlignmentScore + newEdgeAlignmentScore;
             }
             else return null;
