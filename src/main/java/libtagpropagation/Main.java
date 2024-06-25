40e2ef43a2b822f27d93b66945ab0eb9a04d20dc
wyyanhui <137664580+wyyanhui@users.noreply.github.com>
2024-06-25 14:38:11 +0800

    update

--- src/main/java/libtagpropagation/Main.java
+++ src/main/java/libtagpropagation/Main.java
@@ -17,15 +17,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-            String kafkaBroker = "192.168.0.173:9092";
+            String kafkaBroker = "192.168.10.110:9092";
 //            String kafkaBroker = "localhost:9092";
 //            String kafkaBroker = "10.105.189.7:9092";
 //            String kafkaBroker = "192.168.0.173:9092";
 //            String kafkaTopic = "topic-HipsToMrd";
 //            String kafkaTopic = "topic-Linux-malicious";
 //            String kafkaTopic = "topic-CADETS-0-train";
-            String kafkaTopic = "topic-CADETS-0-test";
-//            String kafkaTopic = "topic-CADETS-0";
+//            String kafkaTopic = "topic-CADETS-0-test";
+            String kafkaTopic = "topic-CADETS-0";
             String kafkaGroupId = "mergeAlert";
 
             env.getConfig().registerTypeWithKryoSerializer(PDM.LogPack.class, ProtobufSerializer.class);
@@ -38,8 +38,8 @@ public class Main {
                     .build();
 
             DataStream<PDM.LogPack> logPack_stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-//            EventFrequencyDBConstructionHandler(logPack_stream);
-            AnomalyPathMiningHandler(logPack_stream);
+            EventFrequencyDBConstructionHandler(logPack_stream);
+//            AnomalyPathMiningHandler(logPack_stream);
 
 //        event_stream.keyBy(associatedEvent -> associatedEvent.hostUUID)
 //                .process(new GraphAlignmentProcessFunction());
--- src/main/java/libtagpropagation/ParseTester.java
+++ src/main/java/libtagpropagation/ParseTester.java
@@ -14,7 +14,53 @@ import java.util.Map;
 
 public class ParseTester {
     public static void main(String[] args) throws Exception{
+        ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(Paths.get("./SystemLog/cadetsEventFrequencyDB.out")));
+        Tuple2<Map, Map> dbstring = (Tuple2<Map, Map>) objectInputStream.readObject();
+        Map<AssociatedEvent, HashSet<String>> exactlyMatchEventFrequencyMap = dbstring.f0;
+        Map<AssociatedEvent, HashSet<String>> sourceRelationshipMatchEventFrequencyMap = dbstring.f1;
 
+        // 指定CSV文件的路径
+        String csvFile = "./SystemLog/exactlyEvent-cadets.csv";
+
+        try (FileWriter writer = new FileWriter(csvFile)) {
+            // 写入CSV文件头
+            writer.append("Key,Value\n");
+
+            // 遍历Map并写入CSV
+            for (Map.Entry<AssociatedEvent, HashSet<String>> entry : exactlyMatchEventFrequencyMap.entrySet()) {
+                writer.append(entry.getKey().toString())
+                        .append(",")
+                        .append(entry.getValue().size()+"")
+                        .append("\n");
+            }
+
+            System.out.println("CSV file was created successfully !!!");
+        }catch (IOException e) {
+            System.out.println("An error occurred while writing CSV file.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void durationTime(String startTime, String endTime){
+//         将字符串转换为 BigInteger 对象
+        BigInteger num1 = new BigInteger(startTime);
+        BigInteger num2 = new BigInteger(endTime);
+        String strNum3 = "3600000000000";
+        BigInteger num3 = new BigInteger(strNum3);
+        String strNum4 = "60000000000";
+        BigInteger num4 = new BigInteger(strNum4);
+
+        // 执行大数相减
+//        BigInteger result = num1.divide(num2);
+        BigInteger result = num2.subtract(num1);
+        BigInteger[] results = result.divideAndRemainder(num3);
+
+        // 输出结果
+        System.out.println("Result of subtraction: " + results[0] + " 余： " + results[1].divide(num4));
+    }
+
+    public static void parseDBToCSV() throws IOException, ClassNotFoundException {
         ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(Paths.get("./SystemLog/cadetsEventFrequencyDB.out")));
         Tuple2<Map, Map> dbstring = (Tuple2<Map, Map>) objectInputStream.readObject();
         Map<AssociatedEvent, HashSet<String>> exactlyMatchEventFrequencyMap = dbstring.f0;
@@ -49,24 +95,5 @@ public class ParseTester {
             System.out.println("An error occurred while writing CSV file.");
             e.printStackTrace();
         }
-
-    }
-
-    public static void durationTime(String startTime, String endTime){
-//         将字符串转换为 BigInteger 对象
-        BigInteger num1 = new BigInteger(startTime);
-        BigInteger num2 = new BigInteger(endTime);
-        String strNum3 = "3600000000000";
-        BigInteger num3 = new BigInteger(strNum3);
-        String strNum4 = "60000000000";
-        BigInteger num4 = new BigInteger(strNum4);
-
-        // 执行大数相减
-//        BigInteger result = num1.divide(num2);
-        BigInteger result = num2.subtract(num1);
-        BigInteger[] results = result.divideAndRemainder(num3);
-
-        // 输出结果
-        System.out.println("Result of subtraction: " + results[0] + " 余： " + results[1].divide(num4));
     }
 }
